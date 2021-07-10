54cb618f2abcf1df42a3d90a29c7f5fc82242e0a
QPThree <youngqp3@gmail.com>
2021-07-10 16:00:40 -0700

    acp-ing before I work on loop

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Footer extends React.Component {
   render(){
     return (
-      <p>{ this.props.author}</p>
+      <p>Created By: { this.props.author }</p>
     )
   }
 }
--- src/Main.js
+++ src/Main.js
@@ -8,13 +8,13 @@ class Main extends React.Component{
       <main>
         <h1> {this.props.title}</h1>
         <HornedBeast 
-        title = 'Title 1'
+        title = 'Unicorn and Narwhal'
         source = 'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'
-        description = 'Description 1'/>
+        description = 'Unicorn and Nawhwal say "Hello" '/>
         <HornedBeast 
-        title = 'Title 2'
-        source = 'https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80'
-        desciption = 'Description 2'/>
+        title = 'Rhino'
+        source = "https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80"
+        description = "The Rhino doesn't fall far from the tree"/>
       </main>
 
       
