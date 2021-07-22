afcd3931da757c0d4fdc14047e1ccee0476d6d92
QPThree <youngqp3@gmail.com>
2021-07-21 18:19:31 -0700

    styling

--- src/Main.js
+++ src/Main.js
@@ -7,7 +7,7 @@ import { Container, Row, Col, Form } from 'react-bootstrap';
 //global variables
 
 class Main extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       filterBy: 0,
@@ -40,56 +40,58 @@ class Main extends React.Component {
   render() {
     const Data = this.props.Data;
     let filteredData = [];
-    if (this.state.filterBy === 0){
-      filteredData = Data.map((obj, index) => 
-       <HornedBeast
-        key={index}
-        title={obj.title}
-        imgUrl={obj.image_url}
-        description={obj.description}
-        keyword={obj.keyword}
-        horns={obj.horns}
-        handleShowModal={this.props.handleShowModal}
-        setSelectedBeast={this.props.setSelectedBeast} /> );
+    if (this.state.filterBy === 0) {
+      filteredData = Data.map((obj, index) =>
+        <HornedBeast
+          key={index}
+          title={obj.title}
+          imgUrl={obj.image_url}
+          description={obj.description}
+          keyword={obj.keyword}
+          horns={obj.horns}
+          handleShowModal={this.props.handleShowModal}
+          setSelectedBeast={this.props.setSelectedBeast} />);
     } else {
       filteredData = Data.map((obj, index) => (obj.horns === this.state.filterBy) ?
-      <HornedBeast
-        key={index}
-        title={obj.title}
-        imgUrl={obj.image_url}
-        description={obj.description}
-        keyword={obj.keyword}
-        horns={obj.horns}
-        handleShowModal={this.props.handleShowModal}
-        setSelectedBeast={this.props.setSelectedBeast} /> : false);
+        <HornedBeast
+          key={index}
+          title={obj.title}
+          imgUrl={obj.image_url}
+          description={obj.description}
+          keyword={obj.keyword}
+          horns={obj.horns}
+          handleShowModal={this.props.handleShowModal}
+          setSelectedBeast={this.props.setSelectedBeast} /> : false);
     }
     return (
       <main>
-            <Container fluid>
-        <Row>
-          
+        <Container fluid>
+          <Row>
+
             <Col sm="2">
-              <Form>
+              <Form><span className="formHeader">Filters</span>
                 <Form.Group controlId="exampleForm.SelectCustom">
-                  <Form.Label>Custom select</Form.Label>
-                  <Form.Control as="select" custom onChange = {this.handleFormSelection}>
-                    <option value = "0">All</option>
-                    <option value = "1">1</option>
-                    <option value = "2">2</option>
-                    <option value = "3">3</option>
-                    <option value = "100">4+</option>
+                  <Form.Label className = "formLabel">Horns</Form.Label>
+                  <Form.Control as="select" custom onChange={this.handleFormSelection}>
+                    <option value="0">All</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="100">4+</option>
                   </Form.Control>
                 </Form.Group>
               </Form>
             </Col>
-          
-          <Col sm="10">
-            <h1 className='mainh2'> {this.props.title}</h1>
-            <h2 onClick={this.props.handleShowModal}>Show Modal</h2>
+            
+
+            <Col sm="10">
+              <h1 className='mainh2'> {this.props.title}</h1>
+              <h2 onClick={this.props.handleShowModal}>Show Modal</h2>
               {this.renderGrid(filteredData)}
-          </Col>
-        </Row>
-            </Container>
+            </Col>
+            
+          </Row>
+        </Container>
       </main>
     )
   }
